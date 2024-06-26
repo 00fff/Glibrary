814f413037d143442fe80284265c5841b4110046
00fff <harithsullaiman@gmail.com>
2024-06-26 18:52:57 -0400

    Adjustments for Heroku deployment

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from website import create_app
-from flask import current_app
-from website.apps.cache_config import cache
 
 # Initialize the Flask application
 app = create_app()
 
 if __name__ == "__main__":
+    # Only run the development server when running locally
     app.run(host='0.0.0.0', port=5000)
-
-# Note: No need to explicitly start the app with gunicorn when deploying to Heroku
-# Heroku will use the command specified in Procfile to start the app with gunicorn
