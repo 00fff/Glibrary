ecdbc6e391dd9f316d547750a4185ef5471f8d5a
00fff <harithsullaiman@gmail.com>
2024-06-26 16:19:48 -0400

    docker changes

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from website.apps.cache_config import cache
 app = create_app()
 
 if __name__ == "__main__":
-    # Run the app using Flask's development server (only for local testing)
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000)
 
 # Note: No need to explicitly start the app with gunicorn when deploying to Heroku
 # Heroku will use the command specified in Procfile to start the app with gunicorn
